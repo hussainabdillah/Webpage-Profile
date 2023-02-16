410a44ac8c6d2d506efc04928d03a2bf011886ae
hussainabdillah <hussainkelarno@gmail.com>
2023-02-16 14:54:45 +0700

    addef fitur form submit button

--- js/script.js
+++ js/script.js
@@ -13,3 +13,40 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// fungsi untuk memunculkan message ketika tombol submit ditekan pada form
+
+function message() {
+  var name = document.getElementById("name-input");
+  var email = document.getElementById("email-input");
+  var phone = document.getElementById("phone-input");
+  var message = document.getElementById("msg-input");
+
+  const success = document.getElementById("success");
+  const error = document.getElementById("error");
+
+  if (
+    name.value == "" ||
+    email.value == "" ||
+    phone.value == "" ||
+    message.value == ""
+  ) {
+    error.style.display = "block";
+  } else {
+    setTimeout(() => {
+      name.value = "";
+      email.value = "";
+      phone.value = "";
+      message.value = "";
+    }, 2000);
+
+    success.style.display = "block";
+  }
+
+  setTimeout(() => {
+    error.style.display = "none";
+    success.style.display = "none";
+  }, 3000);
+}
+
+// document.getElementsByClassName("submit-button").onclick = message();
